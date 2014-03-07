185bcd1978ed31d1c10ca2f2e25c4d38aafe89ac
Guillaume <CarteKiwi@users.noreply.github.com>
2014-03-07 11:09:56 +0000

    Inital checkin

---
+++ ToastinetSample/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ToastinetSample.Resources;
+
+namespace ToastinetSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnBasicToast(object sender, RoutedEventArgs e)
+        {
+            this.Toast.Message = "This is a basic toast";
+        }
+
+        private void OnBasicToast2(object sender, RoutedEventArgs e)
+        {
+            this.Toast2.Message = "This is a basic toast with message and without logo. You can set a large text (you have to set height indeed)";
+        }
+    }
+}
