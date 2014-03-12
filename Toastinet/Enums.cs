23c2d2786b56685fdbdf9763a969954c74fb4c18
GDM_cp <SND\GDM_cp>
2014-03-12 11:27:25 +0000

    Delete toastinet reference from package and add refrence to toastinet from solution. Add missing Enum.cs file

---
+++ Toastinet/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toastinet
+{
+    public enum AnimationType
+    {
+        Rotation = 0,
+        Vertical = 1,
+        LeftToRight = 2,
+        RightToLeft = 3,
+        LeftToLeft = 4,
+        RightToRight = 5
+    }
+}
