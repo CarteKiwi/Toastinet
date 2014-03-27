ccc51eb35522ef7c75258ca8304d2e016046a227
Guillaume <CarteKiwi@users.noreply.github.com>
2014-03-27 11:51:51 +0000

    Toastinet for WPF added and fix problem with databinding and multi firing without message change

--- ToastinetWPFSample/MainWindow.xaml.cs
+++ ToastinetWPFSample/MainWindow.xaml.cs
@@ -2,33 +2,38 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Toastinet;
-using ToastinetSample.Resources;
+using System.Windows.Shapes;
+using ToastinetWPF;
 
-namespace ToastinetSample
+namespace ToastinetWPFSample
 {
-    public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
     {
-        private string _firstToastMsg;
-        public string FirstToastMsg
+        private string _message;
+        public string Message
         {
-            get { return _firstToastMsg; }
+            get { return _message; }
             set
             {
-                _firstToastMsg = value;
-                OnPropertyChanged("FirstToastMsg");
+                _message = value;
+                OnPropertyChanged("Message");
             }
         }
 
-        // Constructor
-        public MainPage()
+        public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
@@ -36,7 +41,7 @@ namespace ToastinetSample
 
         private void OnBasicToast(object sender, RoutedEventArgs e)
         {
-            FirstToastMsg = "This is a basic bound toast";
+            this.Message = "This is a basic bound toast";
         }
 
         private void OnBasicToast2(object sender, RoutedEventArgs e)
@@ -49,7 +54,7 @@ namespace ToastinetSample
             this.Toast3.AnimationType = AnimationType.LeftToLeft;
             this.Toast3.Foreground = new SolidColorBrush(Colors.Black);
             this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 230, 126, 34));
-            this.Toast3.Message = "This toast goes from Left To Left";
+            this.Toast3.Message = "This is a toast from left to left";
         }
 
         private void OnR2R(object sender, RoutedEventArgs e)
@@ -57,7 +62,7 @@ namespace ToastinetSample
             this.Toast3.AnimationType = AnimationType.RightToRight;
             this.Toast3.Foreground = new SolidColorBrush(Colors.Black);
             this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 155, 89, 182));
-            this.Toast3.Message = "This toast goes from Right To Right";
+            this.Toast3.Message = "This is a toast from right to right";
         }
 
         private void OnL2R(object sender, RoutedEventArgs e)
@@ -65,7 +70,7 @@ namespace ToastinetSample
             this.Toast3.AnimationType = AnimationType.LeftToRight;
             this.Toast3.Foreground = new SolidColorBrush(Color.FromArgb(255, 155, 89, 182));
             this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 236, 240, 241));
-            this.Toast3.Message = "This toast goes from Left To Right";
+            this.Toast3.Message = "This is a toast from left to right";
         }
 
         private void OnPropertyChanged(string name)
