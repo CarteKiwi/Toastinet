da98db297f1712008d3b828faed01ec84020a3be
GDM_cp <SND\GDM_cp>
2014-04-01 21:22:08 +0000

    add compatibility with Silverlight

---
+++ ToastinetSLSample/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using ToastinetSL;
+
+namespace ToastinetSLSample
+{
+    public partial class MainPage : INotifyPropertyChanged
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void OnBasicToast(object sender, RoutedEventArgs e)
+        {
+            this.Message = "This is a basic bound toast";
+        }
+
+        private void OnBasicToast2(object sender, RoutedEventArgs e)
+        {
+            this.Toast2.Message = "This is a basic toast with message and without logo. You can set a large text (you have to set height indeed)";
+        }
+
+        private void OnL2L(object sender, RoutedEventArgs e)
+        {
+            this.Toast3.AnimationType = AnimationType.LeftToLeft;
+            this.Toast3.Foreground = new SolidColorBrush(Colors.Black);
+            this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 230, 126, 34));
+            this.Toast3.Message = "This is a toast from left to left";
+        }
+
+        private void OnR2R(object sender, RoutedEventArgs e)
+        {
+            this.Toast3.AnimationType = AnimationType.RightToRight;
+            this.Toast3.Foreground = new SolidColorBrush(Colors.Black);
+            this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 155, 89, 182));
+            this.Toast3.Message = "This is a toast from right to right";
+        }
+
+        private void OnL2R(object sender, RoutedEventArgs e)
+        {
+            this.Toast3.AnimationType = AnimationType.LeftToRight;
+            this.Toast3.Foreground = new SolidColorBrush(Color.FromArgb(255, 155, 89, 182));
+            this.Toast3.Background = new SolidColorBrush(Color.FromArgb(255, 236, 240, 241));
+            this.Toast3.Message = "This is a toast from left to right";
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
