bef8107b2866e482fdc3805fa2620daabb048aac
Guillaume <CarteKiwi@users.noreply.github.com>
2015-04-15 13:06:31 +0000

    Remove unused project PCL & old nuget packages from sources

--- Toastinet.WinRT/Toastinet.xaml.cs
+++ Toastinet.WinRT/Toastinet.xaml.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using Windows.Foundation;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
